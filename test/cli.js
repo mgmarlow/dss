8438da152e0b9a431f21cc1eb19a83fa5b7925ad
Graham Marlow <mgmarlow@gmail.com>
2017-12-02 16:59:47 -0800

    add basic composition creation

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,18 @@
 var test = require('tape');
 var cli = require('../lib/cli');
 
-test('cli run returns 0', t => {
-    t.plan(1);
-    t.equal(cli.run(), 0);
+test('create composition adds subject and body', t => {
+    t.plan(2);
+    const rawArgs = ['Beatrice:', 'Then', 'is', 'courtesy', 'a', 'turncoat.'];
+    const composition = cli.createComposition(rawArgs);
+    t.equal(composition.subject, 'Beatrice');
+    t.equal(composition.body, 'Then is courtesy a turncoat.');
+});
+
+test('create composition gives default subject if none provided', t => {
+    t.plan(2);
+    const rawArgs = ['Then', 'is', 'courtesy', 'a', 'turncoat.'];
+    const composition = cli.createComposition(rawArgs);
+    t.equal(composition.subject, 'so and so');
+    t.equal(composition.body, 'Then is courtesy a turncoat.');
 });
