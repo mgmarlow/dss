74614dec35f2f7f1d28e136753e1582f61c96964
Graham Marlow <mgmarlow@gmail.com>
2017-12-03 17:06:30 -0800

    add test for templates

--- test/all.js
+++ test/all.js
@@ -1,1 +1,2 @@
 require('./composition');
+require('./templates');
---
+++ test/templates.js
@@ -0,0 +1,13 @@
+const test = require('tape');
+const templates = require('../lib/templates');
+
+test('parse should fill in mustache keywords on basic letter tpl', t => {
+    t.plan(3);
+    const tpl = 'basic-letter';
+    const comp = { subject: 'Beatrice', body: 'Hello.', author: 'Benedick' };
+    templates.parse(comp, tpl).then(mtpl => {
+        t.assert(mtpl.includes('Beatrice'));
+        t.assert(mtpl.includes('Hello.'));
+        t.assert(mtpl.includes('Benedick'));
+    });
+});
