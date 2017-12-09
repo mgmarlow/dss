b2528f9fc21a2f86f572215d94a5e6e6da05dd51
Graham Marlow <mgmarlow@gmail.com>
2017-12-09 15:11:37 -0800

    refactor configuration

--- test/composition.js
+++ test/composition.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const createComposition = require('../lib/create-composition')
 
-test('composition should properly parse body text', t => {
+test('createComposition should properly parse body text', t => {
   t.plan(1)
   const rawArgs = { '_': ['Beatrice,', 'Then', 'is', 'courtesy', 'a', 'turncoat.'] }
   const text = rawArgs['_']
@@ -9,7 +9,7 @@ test('composition should properly parse body text', t => {
   t.equal(composition.body, 'Then is courtesy a turncoat.')
 })
 
-test('composition should add subject and body', t => {
+test('createComposition should add subject and body', t => {
   t.plan(1)
   const rawArgs = { '_': ['Beatrice,', 'Then', 'is', 'courtesy', 'a', 'turncoat.'] }
   const text = rawArgs['_']
@@ -17,7 +17,7 @@ test('composition should add subject and body', t => {
   t.equal(composition.subject, 'Beatrice')
 })
 
-test('composition should allow for multi-word subjects', t => {
+test('createComposition should allow for multi-word subjects', t => {
   t.plan(2)
   const rawArgs = { '_': ['Jane', 'Doe,', 'Then', 'is', 'courtesy', 'a', 'turncoat.'] }
   const text = rawArgs['_']
@@ -26,7 +26,7 @@ test('composition should allow for multi-word subjects', t => {
   t.equal(composition.body, 'Then is courtesy a turncoat.')
 })
 
-test('composition should throw exception if no subject provided', t => {
+test('createComposition should throw exception if no subject provided', t => {
   t.plan(1)
   const rawArgs = { '_': ['Then', 'is', 'courtesy', 'a', 'turncoat.'] }
   const text = rawArgs['_']
@@ -35,7 +35,7 @@ test('composition should throw exception if no subject provided', t => {
   }, /Please provide a subject. e.g. dear <subject>, <letter body>/)
 })
 
-test('composition should provide author if --yours flag is provided', t => {
+test('createComposition should provide author if --yours flag is provided', t => {
   t.plan(1)
   const rawArgs = {
     '_': ['Beatrice,', 'Then', 'is', 'courtesy', 'a', 'turncoat.'],
@@ -47,7 +47,7 @@ test('composition should provide author if --yours flag is provided', t => {
   t.equal(composition.author, 'Benedick')
 })
 
-test('composition should provide default author "truly"', t => {
+test('createComposition should provide default author "truly"', t => {
   t.plan(1)
   const rawArgs = {
     '_': ['Beatrice,', 'Then', 'is', 'courtesy', 'a', 'turncoat.']
