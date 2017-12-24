1dd79c67e8a39447e949dbfda82a4f269afc482a
Graham Marlow <mgmarlow@gmail.com>
2017-12-24 11:07:19 -0800

    improve test coverage

--- test/amend-duplicate-filename.js
+++ test/amend-duplicate-filename.js
@@ -7,3 +7,18 @@ test('amendFilename should append numbers to duplicate filenames', t => {
   const newFilename = amendFilename(originalFilename, '.js')
   t.equal(newFilename, './test/all(1).js')
 })
+
+test('amendFilename should increment numbers to duplicate filenames', t => {
+  t.plan(1)
+  const originalFilename = './test/files/config(1).json'
+  const newFilename = amendFilename(originalFilename, '.json')
+  t.equal(newFilename, './test/files/config(2).json')
+})
+
+test('amendFilename should work with any extension', t => {
+  t.plan(2)
+  const jsFile = amendFilename('./test/all.js', '.js')
+  const templateFile = amendFilename('./test/files/template.mustache', '.mustache')
+  t.equal(jsFile, './test/all(1).js')
+  t.equal(templateFile, './test/files/template(1).mustache')
+})
